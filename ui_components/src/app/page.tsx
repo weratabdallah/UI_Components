6b3cb0e202dbd632b203301d0da4bedcb175a219
Abdallah <weratabdallah@yahoo.com>
2024-09-17 00:25:12 +0300

    add some edit

--- ui_components/src/app/page.tsx
+++ ui_components/src/app/page.tsx
@@ -1,101 +1,128 @@
-import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { SiReact } from "react-icons/si"; // {{ edit_1 }}
+import StorageRoundedIcon from '@mui/icons-material/StorageRounded'; // {{ edit_1 }}
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div className="poppins">
+      <Navbar />
+      <CutSection />
+      <Features />
     </div>
+  );
+}
+
+function Navbar() {
+  return (
+    <nav className="flex m-5 max-sm:mt-9 mx-8 items-center justify-between max-sm:flex-col">
+      <Logo />
+      <Button />
+    </nav>
+  );
+}
+
+function Logo() {
+  return (
+    <div className="flex gap-2 items-center">
+      <div className="bg-sky-500 flex items-center justify-center p-[6px] rounded-md ">
+        <div className="w-[26px] h-[26px] items-center justify-center flex">
+          <SiReact className="text-white text-[22px]" />
+        </div>
+      </div>
+      <div className="flex gap-1 text-[22px] ">
+        <span className="font-bold text-sky-500">UI</span>
+        <span className="text-slate-600">Shelf</span>
+      </div>
+    </div>
+  );
+}
+
+function Button() {
+  return (
+    <div className="flex gap-2 max-sm:flex-col max-sm:w-full max-sm:mt-8">
+      <Link href="/sign-in">
+        <button
+          className=" max-sm:w-full text-sm border border-sky-500 text-white bg-sky-500 
+ p-[8px] px-6 rounded-md "
+        >
+          Sign In
+        </button>
+      </Link>
+      <Link href="/sign-up">
+        <button
+          className=" max-sm:w-full text-sm border border-sky-500 text-sky-500 
+hover:bg-sky-500 hover:text-white p-[8px] px-6 rounded-md "
+        >
+          Sign Up
+        </button>
+      </Link>
+    </div>
+  );
+}
+
+function CutSection() {
+  return (
+    <div className="flex flex-col mx-16 items-center mt-[120px] gap-6 ">
+      <h2 className="font-bold text-2xl text-center">
+        Manage and Create Your UI Components
+        <span className="text-sky-500"> Effortlessly!</span>
+      </h2>
+      <p className="text-center text-[15px] w-[510px] max-sm:w-full text-slate-500 ">
+        Save time by reusing your favorite components. Store them in a
+        centralized database and create new components with ease. Enhance your
+        development workflow by having quick access to a library of reusable
+        components and ensure consistency across your projects.
+      </p>
+      <button
+        className="block bg-sky-500 rounded-md  px-9 py-3 text-sm font-medium text-white hover:bg-sky-600    "
+        type="button"
+      >
+        Let&apos;s get started!
+      </button>
+    </div>
+  );
+}
+
+function Features() {
+  const feature = [
+    {
+      id: 1,
+      name: "Centralized Component Library",
+      icon: <StorageRoundedIcon className="text-sky-500 text-[32px]" />, // {{ edit_1 }}
+      description: "Organize all your React components in a centralized library. Easily browse, search, and access your saved components whenever you need them"
+    },
+    {
+      id: 2,
+      name: "Reusable Components",
+      icon: <StorageRoundedIcon className="text-sky-500 text-[32px]" />, // {{ edit_1 }}
+      description: "Create and edit your React components directly within our intuitive editor. Write JSX code with syntax highlighting and instant previews."
+    },
+    {
+      id: 3,
+      name: "Instant Previews",
+      icon: <StorageRoundedIcon className="text-sky-500 text-[32px]" />, // {{ edit_1 }}
+      description: "See your components come to life instantly with our live preview feature. Test and iterate on your components in real-time."
+    }
+  ];
+
+  return (
+    <section className=" py-12 bg-slate-50 mt-12">
+      <div className=" mx-auto px-4 ">
+        <h2 className="text-2xl font-bold text-center ">Key Features</h2>
+        <div className="mt-12 grid grid-cols-1 md:grid-cols-3 gap-8 ">
+          {feature.map((item) => (
+            <div key={item.id} className=" p-6 bg-white rounded-lg shadow-sm flex flex-col items-center ">
+              <h3 className="text-lg font-semibold text-sky-500 mt-6 text-center">
+                {item.name}
+              </h3>
+              <p className="text-slate-600 text-[13px] mt-2 text-center w-[80%]">
+                {item.description}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
   );
 }
